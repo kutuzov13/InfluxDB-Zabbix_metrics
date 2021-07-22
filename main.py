ce1be2bc1e4058d5ae9fbc1e9ba276845a7796ef
Yegor <kutuzov-1313@yandex.ru>
2021-07-22 17:57:30 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import time
-from influxdb import InfluxDBClient
 from datetime import datetime
+
+from influxdb.exceptions import InfluxDBClientError
+from influxdb import InfluxDBClient
 from zabbix import Zabbix
 
 INFLUXDB_HOST = os.getenv('INFLUXDB_HOST')
@@ -10,7 +12,7 @@ INFLUXDB_PORT = 8086
 
 
 def get_triggers():
-    """Get all triggers on Zabbix"""
+    """Get all triggers on Zabbix."""
     zabbix = Zabbix()
     all_triggers = list()
     for trigger in zabbix.triggered():
@@ -19,31 +21,32 @@ def get_triggers():
 
 
 def influx_send_metrics(metrics_name, metrics_value):
-    """Connecting to the influxDB"""
+    """Connecting to the influxDB."""
     client = InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT)
-    client.create_database(INFLUXDB_DATABASE)
-    client.switch_database(INFLUXDB_DATABASE)
-    client.alter_retention_policy(
-        name='autogen',
-        duration='7d',
-        database=INFLUXDB_DATABASE,
-        default=False,
-        shard_duration='7d'
-    )
-    current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-    add_metrics_hardware = [
-        {
-            'measurement': f'{metrics_name}',
-            'tags': {
-                'Name': metrics_name,
-            },
-            'time': current_time,
-            'fields': {
-                "Value": metrics_value,
-            },
-        },
-    ]
     try:
+        client.create_database(INFLUXDB_DATABASE)
+        client.alter_retention_policy(
+            name='autogen',
+            duration='7d',
+            database=INFLUXDB_DATABASE,
+            default=False,
+            shard_duration='7d'
+        )
+        current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        add_metrics_hardware = [
+            {
+                'measurement': f'{metrics_name}',
+                'tags': {
+                    'Name': metrics_name,
+                },
+                'time': current_time,
+                'fields': {
+                    "Value": metrics_value,
+                },
+            },
+        ]
         client.write_points(add_metrics_hardware)
-    except ConnectionError:
-        time.sleep(300)
+        
+    except InfluxDBClientError:
+    client.create_database(INFLUXDB_DB)
+    client.close()
